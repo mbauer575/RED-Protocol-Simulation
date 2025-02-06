1f2528807c9ad51d313d9c4a7cfc0e66de2407a6
mbauer575 <bauermax@oregonstate.edu>
2025-02-06 12:39:12 -0800

    def

--- red-sim.py
+++ red-sim.py
@@ -5,28 +5,61 @@ import random
 
 # Node class
 
+
+
 class Node: 
-    def __init__(self, node_id):
-        return None
-    
+    # list of links
+    # node coordinates
     
 
 
 
 # Host class (node)
 class Host(Node):
-
-
+    # bool sending packets or not (on/off)
+    # ack 
+    # most reacent ack
+    # tick function
+    # udp queue
+    # number of packets sent
+    # number of packets received
+    # (each host)
+    # rtt 
+    # retransmission timer
+    # tcp queue
+    # window size
+    # devrtt
+    # rto
 
 
 # Router class (node)
 class Router(Node):
-
+    # dict of links
+    # routing table
+    # routing algorithm
+    # packet queue
+    # packet buffer
+    # tick function
+    # average queue length
 
 # Packet class
 class Packet:
-
+    # type udp or tcp
+    # soruce
+    # destination
+    # ack bit
+    # sequence number
 
 
 # Link class
 class Link:
+    # fixed propogation delay
+    # packet 
+    # nodes conencted (2)
+    # direction
+    # tick function
+
+
+
+def simulation
+
