1d437a6a970926859db07399c27bc0ab7326bbd3
mbauer575 <bauermax@oregonstate.edu>
2025-02-06 12:21:22 -0800

    class def

---
+++ red-sim.py
@@ -0,0 +1,32 @@
+import numpy as np
+import random
+
+# Red Protocol Simulation
+
+# Node class
+
+class Node: 
+    def __init__(self, node_id):
+        return None
+    
+    
+
+
+
+# Host class (node)
+class Host(Node):
+
+
+
+
+# Router class (node)
+class Router(Node):
+
+
+# Packet class
+class Packet:
+
+
+
+# Link class
+class Link:
